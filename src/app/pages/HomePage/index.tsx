e44b33750c4266943e14a555365f3e732335a881
sol <sadlthf@icloud.com>
2023-02-23 21:38:54 +0900

    feature: page layout, todo box base
    * change notFound page location

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,14 +1,61 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #eee;
+`;
+
+const Box = styled.div`
+  width: 400px;
+  height: 600px;
+  background: white;
+  box-shadow: 0xp 25px 100px -69px rgba(0, 0, 0, 0.18);
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 15px 25px;
+`;
+
+const TodoList = styled.div``;
+
+const TodoItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  width: 100%;
+  font-size: 1.2em;
+  border-bottom: 1px solid #eee;
+`;
+
+const TodoCheck = styled.input`
+  margin-right: 15px;
+`;
 
 export function HomePage() {
   return (
     <>
       <Helmet>
-        <title>HomePage</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <title>Main</title>
+        <meta name="description" content="TO-DO Main App" />
       </Helmet>
-      <span>My HomePage</span>
+      <Wrapper>
+        <Box>
+          <Title>할 일</Title>
+          <TodoList>
+            <TodoItem>
+              <TodoCheck type="checkbox" />
+              나는 투두야
+            </TodoItem>
+          </TodoList>
+        </Box>
+      </Wrapper>
     </>
   );
 }
